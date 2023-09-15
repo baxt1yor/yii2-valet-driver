26e3807cb6b30c969aab6814583c88c3f5bfdefa
Baxtiyor Eshametov <52046670+baxt1yor@users.noreply.github.com>
2023-09-15 18:21:57 +0000

    Update Yii2ValetDriver.php

--- Yii2ValetDriver.php
+++ Yii2ValetDriver.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace Valet\Drivers\Specific;
 
 use Valet\Drivers\ValetDriver;
+
 class Yii2ValetDriver extends ValetDriver
 {
     /**
@@ -32,7 +32,13 @@ class Yii2ValetDriver extends ValetDriver
      */
     public function isStaticFile(string $sitePath, string $siteName, string $uri)
     {
-        if (file_exists($staticFilePath = $sitePath.'/web/'.$uri)) {
+        if(function_exists('str_starts_with') && str_starts_with($siteName, "assets")) {
+            return $sitePath.$uri;
+        } elseif(preg_match("#^assets#", $siteName) ) { // this line for php <= 7.4
+            return $sitePath.$uri;
+        }
+
+        if (file_exists($staticFilePath = $sitePath.'/web/'.$uri) && ! is_dir ( $staticFilePath ) && pathinfo ( $staticFilePath )['extension'] != '.php') {
             return $staticFilePath;
         }
 
@@ -40,8 +46,6 @@ class Yii2ValetDriver extends ValetDriver
     }
 
     /**
-     * Get the fully resolved path to the application's front controller.
-     *
      * @param string $sitePath
      * @param string $siteName
      * @param string $uri
@@ -49,6 +53,20 @@ class Yii2ValetDriver extends ValetDriver
      */
     public function frontControllerPath(string $sitePath, string $siteName, string $uri): ?string
     {
+
+        $uri_path = explode('/',$uri)[1];
+
+
+        if (file_exists($sitePath.'/web/'. $uri_path . '/index.php') && !empty($uri_path)) {
+
+            $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/web/' . $uri_path . '/index.php';
+            $_SERVER['SCRIPT_NAME'] = '/' . $uri_path . '/index.php';
+            $_SERVER['PHP_SELF'] = '/' . $uri_path . '/index.php';
+            $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+
+            return $sitePath.'/web/' . $uri_path . '/index.php';
+        }
+
         $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
         $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/web/index.php';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
